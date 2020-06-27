a1d9b9acd88166fa7d9f5406edc1b46f86e6c396
juandrojas <juandrojas33@gmail.com>
2020-06-26 19:14:34 -0500

    se agregó otra pregunta, en el ejecutable hay métodos que escriben el .tex. Tal como está, ele ejectuable crea un archivo .tex con las preguntas creadas

--- executable.py
+++ executable.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
 import question, subprocess, os, random
-p = question.Pregunta(1)
-with open('file.text', 'w') as file:
+
+def write_in_text(file, p):
 	file.write('------------------------------\n')
 	file.write('\n')
 	file.write('Title:\n')
 	file.write(p.text + '\n')
 	file.write('% Juan Diego Rojas, Tipo 0' + str(p.tipo) +', ver 01\n')
 	file.write('\n')
-	random.shuffle(p.opciones)
-	file.write(p.opciones[0].asterisco() +  'a. ' + str(p.opciones[0].valor) + '\n')
-	file.write(p.opciones[1].asterisco() +  'b. ' + str(p.opciones[1].valor) + '\n')
-	file.write(p.opciones[2].asterisco() +  'c. ' + str(p.opciones[2].valor) + '\n')
-	file.write(p.opciones[3].asterisco() +  'd. ' + str(p.opciones[3].valor) + '\n')
+	random.shuffle(p.opcionestext)
+	file.write(p.opcionestext[0].asterisco() +  'a. ' + p.opciones[0].valor + '\n')
+	file.write(p.opcionestext[1].asterisco() +  'b. ' + p.opciones[1].valor + '\n')
+	file.write(p.opcionestext[2].asterisco() +  'c. ' + p.opciones[2].valor + '\n')
+	file.write(p.opcionestext[3].asterisco() +  'd. ' + p.opciones[3].valor + '\n')
+	file.write('------------------------------\n')
+
+def write_in_tex(file, p):
+	file.write('\\item ' + p.tex + '\n')
+	file.write('\\begin{enumerate}[label = (\\alph*)]\n')
+	random.shuffle(p.opcionestex)
+	file.write('\\item '+ p.opcionestex[0].valor + '\n')
+	file.write('\\item '+ p.opcionestex[1].valor + '\n')
+	file.write('\\item '+ p.opcionestex[2].valor + '\n')
+	file.write('\\item '+ p.opcionestex[3].valor + '\n')
+	file.write('\\end{enumerate}\n')
+
+def tex_preamble(file):
+	file.write('\\documentclass[11pt]{article}\n')
+	file.write('\\usepackage[utf8]{inputenc}\n')
+	file.write('\\usepackage[spanish]{babel}\n')
+	file.write('\\usepackage{blindtext}\n')
+	file.write('\\usepackage{enumitem}\n')
+	file.write('\\begin{document}\n')
+	file.write('\\begin{enumerate}\n')
+
+file = open('main.tex', 'w')
+tex_preamble(file)
+for i in range(1,3):
+	write_in_tex(file, question.Pregunta(i))
+file.write('\\end{enumerate}\n')
+file.write('\\end{document}\n')
+
+
--- question.py
+++ question.py
@@ -10,6 +10,9 @@ class Tipo:
 		if tipo == 1:
 			self.seccion = '4.4'
 			self.numero = '39'
+		elif tipo == 2:
+			self.seccion = '4.4'
+			self.numero = '4.47b'
 
 class Respuesta:
 	"""docstring for Respuesta"""
@@ -26,15 +29,33 @@ class Pregunta:
 	"""base para cada pregunta for Pregunta"""
 	def __init__(self, tipo):
 		self.tipo = tipo
-		self.opciones = []
+		self.opcionestext = []
+		self.opcionestex = []
 		if tipo == 1:
 			a = random.randint(2, 5)
 			self.tex = 'Si un paracaidista aterriza en un punto aleatorio en una recta entre los marcadores $A$ y $B$, encuentre la probabilidad de que su distancia hasta $A$ sea más de ' + num2words(a, lang='es') + ' veces su distancia a $B$.'
 			self.text = 'Si un paracaidista aterriza en un punto aleatorio en una recta entre los marcadores A y B, encuentre la probabilidad de que su distancia hasta A sea más de ' + num2words(a, lang='es') + ' veces su distancia a B.'
-			self.opciones.append(Respuesta(st.uniform.cdf(1/(a +1)), True))
-			self.opciones.append(Respuesta(st.uniform.cdf(1/a), False))
-			self.opciones.append(Respuesta(1 - st.uniform.cdf(1/(a+1)), False))
-			self.opciones.append(Respuesta(1 - st.uniform.cdf(1/a), False))
-		else:
-			pass
+			self.opcionestext.append(Respuesta(str(st.uniform.cdf(1/(a +1))), True))
+			self.opcionestext.append(Respuesta(str(st.uniform.cdf(1/a)), False))
+			self.opcionestext.append(Respuesta(str(1 - st.uniform.cdf(1/(a+1))), False))
+			self.opcionestext.append(Respuesta(str(1 - st.uniform.cdf(1/a)), False))
+			self.opcionestex.append(Respuesta(str(st.uniform.cdf(1/(a +1))), True))
+			self.opcionestex.append(Respuesta(str(st.uniform.cdf(1/a)), False))
+			self.opcionestex.append(Respuesta(str(1 - st.uniform.cdf(1/(a+1))), False))
+			self.opcionestex.append(Respuesta(str(1 - st.uniform.cdf(1/a)), False))
+		elif tipo == 2:
+			a = random.randint(1,5)
+			i = random.randint(1,5)
+			b = a+i
+			self.tex = 'La falla de una tarjeta de circuito que utiliza un sistema de cómputo interrumpe el trabajo hasta que se instala una nueva. El tiempo de entrega, $Y$, está uniformemente distribuido en el intervalo de ' + num2words(a, lang='es') +' a ' + num2words(b, lang='es')+ ' días. El costo de la falla de una tarjeta y la interrupción incluye el costo fijo $c_0$ de una nueva tarjeta y un costo que aumenta proporcionalmente con $Y^2$ .Si $C$ es el costo en que se incurre, $C=c_0+c_1Y^2$, en términos de $c_0$ y $c_1$ encuentre el costo esperado asociado a que una sola tarjeta de circuito que falle.'
+			self.text = 'La falla de una tarjeta de circuito que utiliza un sistema de cómputo interrumpe el trabajo hasta que se instala una nueva. El tiempo de entrega, Y, está uniformemente distribuido en el intervalo de ' + num2words(a, lang='es') +' a ' + num2words(b, lang='es')+ ' días. El costo de la falla de una tarjeta y la interrupción incluye el costo fijo c_0 de una nueva tarjeta y un costo que aumenta proporcionalmente con Y^2 .$ Si C es el costo en que se incurre, C=c_0+c_1*Y^2, , en términos de c_0 y c_1 encuentre el costo esperado asociado a que una sola tarjeta de circuito que falle.' 
+			self.opcionestex.append(Respuesta('$c_0 + ' + str(st.uniform.var(loc=a, scale =i) + st.uniform.mean(loc=a, scale =i)**2) + 'c_1$', True))
+			self.opcionestex.append(Respuesta('$c_0 + ' + str(st.uniform.mean(loc=a, scale =i)**2) + 'c_1$', False))
+			self.opcionestex.append(Respuesta('$c_0 + ' + str(st.uniform.mean(loc=a, scale =i)) + 'c_1$', False))
+			self.opcionestex.append(Respuesta('$c_0 + ' + str(st.uniform.var(loc=a, scale =i)) + 'c_1$', False))
+			self.opcionestext.append(Respuesta('c_0 + ' + str(st.uniform.var(loc=a, scale =i) + st.uniform.mean(loc=a, scale =i)**2) + '*c_1', True))
+			self.opcionestext.append(Respuesta('c_0 + ' + str(st.uniform.mean(loc=a, scale =i)**2) + '*c_1', False))
+			self.opcionestext.append(Respuesta('c_0 + ' + str(st.uniform.mean(loc=a, scale =i)) + '*c_1', False))
+			self.opcionestext.append(Respuesta('c_0 + ' + str(st.uniform.var(loc=a, scale =i)) + '*c_1', False))
+
 
